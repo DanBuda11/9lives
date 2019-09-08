efa57c69eaced4d0f7b971d7e0b6d120af43a5f4
Dan Buda <danbuda@gmail.com>
2019-09-07 23:37:55 -0500

    add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-styled-components',
     `gatsby-plugin-favicon`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
